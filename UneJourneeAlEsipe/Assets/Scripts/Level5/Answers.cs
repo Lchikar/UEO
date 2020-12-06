fd7acedb260628b5b35557ea51f3228124da8664
LafLaurine <laurine.lafontaine@outlook.fr>
2020-12-06 16:15:03 +0100

    [Level 5] - Set quiz and answers
    todo : feedback on color button

---
+++ UneJourneeAlEsipe/Assets/Scripts/Level5/Answers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Answers : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+    public void Answer()
+    {
+        if(isCorrect)
+        {
+            Debug.Log("Correct answer");
+            /*var colors = quizManager.m_options[quizManager.m_indexAnswer].GetComponent<Button>().colors;
+            colors.normalColor = Color.green;
+            quizManager.m_options[quizManager.m_indexAnswer].GetComponent<Button>().colors = colors;*/
+            quizManager.correct();
+        } else
+        {
+            Debug.Log("Wrong answer");
+            quizManager.wrong();
+        }
+    }
+}
---
+++ UneJourneeAlEsipe/Assets/Scripts/Level5/Database.cs
@@ -0,0 +1,8 @@
+﻿
+[System.Serializable]
+public class Database 
+{
+    public string m_questions;
+    public string[] m_answers;
+    public int m_correctAnswer;
+}
