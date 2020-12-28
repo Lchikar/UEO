782d1e662ecf415c194906c0061acbd23272f4b6
Louisa <louisa.chikar@gmail.com>
2020-12-28 16:10:57 +0100

    [Level 2] - Add first setup

---
+++ UneJourneeAlEsipe/Assets/Scenes/2-Petit-dejeuner/Temp-scripts/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public KeyCode keyToPress;
+    public float rescale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress)){
+           transform.localScale *= rescale;
+        }
+
+        if(Input.GetKeyUp(keyToPress)){
+           transform.localScale /= rescale;
+        }
+    }
+}
