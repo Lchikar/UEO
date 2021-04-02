729e68ab591eb66aaeb9eaf3b57c316198e88a15
LafLaurine <laurine.lafontaine@outlook.fr>
2021-04-02 11:03:45 +0200

    [Level4] - Done hallway level and fix other levels

--- UneJourneeAlEsipe/Assets/Scenes/1-Reveil/Scripts/QTE.cs
+++ UneJourneeAlEsipe/Assets/Scenes/1-Reveil/Scripts/QTE.cs
@@ -53,22 +53,18 @@ public class QTE : MonoBehaviour
             {
                 case 1:
                     m_waitingKey = 1;
-                    //displayBox.GetComponent<Text>().text = "[E]";
                     displayBox.GetComponent<Image>().overrideSprite = Efull;
                     break;
                 case 2:
                     m_waitingKey = 2;
-                    //displayBox.GetComponent<Text>().text = "[S]";
                     displayBox.GetComponent<Image>().overrideSprite = Sfull;
                     break;
                 case 3:
                     m_waitingKey = 3;
-                    //displayBox.GetComponent<Text>().text = "[I]";
                     displayBox.GetComponent<Image>().overrideSprite = Ifull;
                     break;
                 case 4:
                     m_waitingKey = 4;
-                    //displayBox.GetComponent<Text>().text = "[P]";
                     displayBox.GetComponent<Image>().overrideSprite = Pfull;
                     break;
                 default:
@@ -127,8 +123,7 @@ public class QTE : MonoBehaviour
 
         if(m_score == 5)
         {
-            gameHandler.LoadLevel("Demo-Ending");
-            //gameHandler.LoadLevel("Petit-dejeuner");
+            gameHandler.LoadLevel("Petit-dejeuner");
         }
     }
 
@@ -154,7 +149,6 @@ public class QTE : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         m_correctKey = 0;
         m_passBox.GetComponent<Text>().text = "";
-        //displayBox.GetComponent<Text>().text = "";
         displayBox.GetComponent<Image>().sprite = defaultSprite;
         yield return new WaitForSeconds(1.5f);
         m_waitingKey = 0;
@@ -178,7 +172,6 @@ public class QTE : MonoBehaviour
             yield return new WaitForSeconds(1.5f);
             m_correctKey = 0;
             m_passBox.GetComponent<Text>().text = "";
-            //displayBox.GetComponent<Text>().text = "";
             displayBox.GetComponent<Image>().sprite = defaultSprite;
             yield return new WaitForSeconds(1.5f);
             m_waitingKey = 0;
---
+++ UneJourneeAlEsipe/Assets/Scenes/4-Couloirs/Scripts/DialogManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public class Sentences
+{
+	public string narratorName;
+	[TextArea(10, 20)]
+	public string text;
+	public UnityEvent dialogStartedCallback;
+}
+
+[System.Serializable]
+public class Dialog
+{
+	public Sentences[] sentences;
+	[Tooltip("in letters by seconds")] public float typingSpeed = 20.0f;
+}
+
+
+public class DialogManager : MonoBehaviour
+{
+
+	[SerializeField] Text nameText;
+	[SerializeField] Text dialogText;
+	[SerializeField] Animator dialogHUD;
+	[SerializeField] AudioSource dialogSound;
+	[SerializeField] UnityEngine.UI.Text UITextNarrator;
+	[SerializeField] UnityEngine.UI.Text UITextDialog;
+	[SerializeField] Dialog dialogToPlay;
+	[SerializeField] UnityEvent dialogEndedCallback;
+
+	public static DialogManager currentOpendialog = null;
+
+	private bool dialogueStarted = false;
+	private bool displayNextSentence = false;
+
+	public void StartDialog()
+	{
+		if (!dialogueStarted)
+		{
+			if (currentOpendialog != null)
+			{
+				currentOpendialog.StopAllCoroutines();
+				currentOpendialog.dialogHUD.SetBool("open", false);
+				currentOpendialog.dialogueStarted = false;
+			}
+			currentOpendialog = this;
+			dialogueStarted = true;
+			StartCoroutine(DisplayDialog(dialogToPlay));
+		}
+	}
+
+    private void Awake()
+    {
+		StartDialog();
+	}
+
+	IEnumerator TypeSentence(UnityEngine.UI.Text textObj, string sentence, float secByletter)
+	{
+		textObj.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogSound.Play();
+			textObj.text += letter;
+			yield return new WaitForSeconds(secByletter);
+		}
+		dialogSound.Stop();
+	}
+
+	IEnumerator DisplayDialog(Dialog dialog)
+	{
+		UITextNarrator.text = dialog.sentences[0].narratorName;
+		UITextDialog.text = "";
+		dialogHUD.SetBool("open", true);
+		yield return new WaitForSeconds(0.5f);
+
+		foreach (var sentence in dialog.sentences)
+		{
+			sentence.dialogStartedCallback.Invoke();
+			UITextNarrator.text = sentence.narratorName;
+			yield return StartCoroutine(TypeSentence(UITextDialog, sentence.text, 1 / dialog.typingSpeed));
+			displayNextSentence = false;
+
+			while (!displayNextSentence) yield return null;
+		}
+		dialogEndedCallback.Invoke();
+		dialogHUD.SetBool("open", false);
+
+		dialogueStarted = false;
+
+	}
+
+	public void NextSentence()
+    {
+		displayNextSentence = true;
+	}
+}
